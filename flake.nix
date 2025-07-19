1f87da82fa25bd91fea9ffb0d90a5ac0826edeb3
nuqor <github@nuqor.de>
2025-07-19 23:36:05 +0200

    Add support for other systems

--- flake.nix
+++ flake.nix
@@ -6,66 +6,81 @@
   };
 
   outputs =
-    { nixpkgs, ... }:
+    { self, nixpkgs, ... }:
     let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      plugins = with pkgs.vimPlugins; [
-        catppuccin-nvim
-        conform-nvim
-        fidget-nvim
-        gitsigns-nvim
-        lualine-nvim
-        neo-tree-nvim
-        noice-nvim
-        nvim-lint
-        nvim-lspconfig
-        nvim-treesitter.withAllGrammars
-        nvim-web-devicons
-        render-markdown-nvim
-        smart-splits-nvim
-        snacks-nvim
-        telescope-fzf-native-nvim
-        telescope-nvim
-        tiny-inline-diagnostic-nvim
-        vim-slime
+      systems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
       ];
-      runtimeDeps = with pkgs; [
-        lemminx
-        markdownlint-cli
-        marksman
-        mdformat
-        nixd
-        nixfmt-rfc-style
-        pyright
-        ripgrep
-        ruff
-        stylua
-        tombi
-        vscode-langservers-extracted
-        yaml-language-server
-      ];
-      multivimPlugin = pkgs.vimUtils.buildVimPlugin {
-        name = "multivim";
-        src = ./multivim;
-      };
-      # extraPython3Packages = python-packages: with python-packages; [ ipython ];
+      eachSystem =
+        f: (nixpkgs.lib.attrsets.genAttrs systems (system: f system nixpkgs.legacyPackages.${system}));
+      plugins =
+        pkgs: with pkgs.vimPlugins; [
+          catppuccin-nvim
+          conform-nvim
+          fidget-nvim
+          gitsigns-nvim
+          lualine-nvim
+          neo-tree-nvim
+          noice-nvim
+          nvim-lint
+          nvim-lspconfig
+          nvim-treesitter.withAllGrammars
+          nvim-web-devicons
+          render-markdown-nvim
+          smart-splits-nvim
+          snacks-nvim
+          telescope-fzf-native-nvim
+          telescope-nvim
+          tiny-inline-diagnostic-nvim
+          vim-slime
+        ];
+      runtimeDeps =
+        pkgs: with pkgs; [
+          lemminx
+          markdownlint-cli
+          marksman
+          mdformat
+          nixd
+          nixfmt-rfc-style
+          pyright
+          ripgrep
+          ruff
+          stylua
+          tombi
+          vscode-langservers-extracted
+          yaml-language-server
+        ];
+      multivimPlugin =
+        pkgs:
+        pkgs.vimUtils.buildVimPlugin {
+          name = "multivim";
+          src = ./multivim;
+        };
     in
     {
 
-      packages.${system}.default =
-        (pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped {
-          plugins = [ multivimPlugin ] ++ plugins;
-          luaRcContent = builtins.readFile ./init.lua;
-          wrapperArgs = "--set MPL_BACKENDS_PATH ${./multivim/python/mpl_backends}";
-          # withPython3 = true;
-          # inherit extraPython3Packages;
-        }).overrideAttrs
-          (
-            finalAttrs: previousAttrs: {
-              runtimeDeps = previousAttrs.runtimeDeps ++ runtimeDeps;
-            }
+      packages = eachSystem (
+        system: pkgs: {
+          default = self.packages.${system}.neovim;
+          neovim = (
+            (pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped {
+              plugins = [
+                (multivimPlugin pkgs)
+              ] ++ (plugins pkgs);
+              luaRcContent = builtins.readFile ./init.lua;
+              wrapperArgs = "--set MPL_BACKENDS_PATH ${./multivim/python/mpl_backends}";
+            }).overrideAttrs
+              (
+                finalAttrs: previousAttrs: {
+                  runtimeDeps = previousAttrs.runtimeDeps ++ (runtimeDeps pkgs);
+                }
+              )
           );
+        }
+      );
 
     };
 

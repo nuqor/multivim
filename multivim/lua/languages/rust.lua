5fc0799ba9471d29b8a936899e9cbf75699cf49c
nuqor <github@nuqor.de>
2025-09-12 18:51:51 +0200

    Add rust_analyzer

---
+++ multivim/lua/languages/rust.lua
@@ -0,0 +1,34 @@
+local function format(bufnr)
+  vim.lsp.buf.format {
+    bufnr = bufnr,
+    filter = function(client)
+      return client.name == "rust_analyzer"
+    end,
+  }
+end
+
+local function format_on_save_callback(args)
+  if vim.b[args.buf].format_on_save then
+    format(args.buf)
+  end
+end
+
+vim.lsp.enable("rust_analyzer")
+vim.lsp.config("rust_analyzer", {
+  on_attach = function(client, bufnr)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      callback = format_on_save_callback,
+    })
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = "nix",
+  callback = function(args)
+    vim.opt.tabstop = 4
+    vim.opt.softtabstop = 4
+    vim.opt.shiftwidth = 4
+    vim.b[args.buf].format_on_save = true
+  end,
+})

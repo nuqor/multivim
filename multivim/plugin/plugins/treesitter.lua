29a8e377b302cabe1dd4ca941b420439c9a89828
nuqor <github@nuqor.de>
2025-09-19 22:34:34 +0200

    Update nvim-treesitter

--- multivim/plugin/plugins/treesitter.lua
+++ multivim/plugin/plugins/treesitter.lua
@@ -1,7 +1,15 @@
-require("dependencies").add { source = "nvim-treesitter/nvim-treesitter" }
+require("dependencies").add {
+  source = "nvim-treesitter/nvim-treesitter",
+  hooks = {
+    post_checkout = function()
+      vim.cmd("TSUpdate")
+    end,
+  },
+}
 
 -- TODO: test folding and incremental selection
 require("nvim-treesitter.configs").setup {
+  auto_install = not require("dependencies").is_nix_native(),
   highlight = { enable = true },
   -- indent = { enable = true },
 }

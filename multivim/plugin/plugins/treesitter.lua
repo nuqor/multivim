cb005133334f6800b6ff7a6a944a74b5fdec97d6
nuqor <github@nuqor.de>
2025-09-20 19:42:41 +0200

    Add ensure_installed for treesiter

--- multivim/plugin/plugins/treesitter.lua
+++ multivim/plugin/plugins/treesitter.lua
@@ -7,9 +7,49 @@ require("dependencies").add {
   },
 }
 
--- TODO: test folding and incremental selection
-require("nvim-treesitter.configs").setup {
+config = {
   auto_install = not require("dependencies").is_nix_native(),
   highlight = { enable = true },
   -- indent = { enable = true },
 }
+
+if not require("dependencies").is_nix_native() then
+  config["ensure_installed"] = {
+    "bash",
+    "c",
+    "cmake",
+    "cpp",
+    "css",
+    "csv",
+    "dockerkfile",
+    "git_config",
+    "gitcommit",
+    "gitignore",
+    "html",
+    "ini",
+    "javascript",
+    "json",
+    "json_schema",
+    "jsonc",
+    "just",
+    "latex",
+    "make",
+    "markdown",
+    "markdown_inline",
+    "nu",
+    "python",
+    "regex",
+    "requirements",
+    "rust",
+    "scss",
+    "tsv",
+    "tsx",
+    "typescript",
+    "typst",
+    "xml",
+    "yaml",
+  }
+end
+
+-- TODO: test folding and incremental selection
+require("nvim-treesitter.configs").setup(config)
